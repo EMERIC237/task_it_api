d9a559475845d80295ac2111bb7128f09cfbd900
Emeric FOKOUONG TOUOLAC <bestvemeric@gmail.com>
2024-03-28 20:17:47 -0500

    made user_id  non null on review

--- db/migrate/20240214035653_create_reviews.rb
+++ db/migrate/20240214035653_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[7.1]
   def change
     create_table :reviews do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: true, foreign_key: true
       t.datetime :date
       t.text :summary
       t.text :action_items
--- db/migrate/20240227183044_make_user_id_optional_in_reviews.rb
+++
@@ -1,5 +0,0 @@
-class MakeUserIdOptionalInReviews < ActiveRecord::Migration[7.1]
-  def change
-    change_column_null :reviews, :user_id, true
-  end
-end
